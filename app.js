9c752abeb2ef7755c4be47d6877fe2948276088e
jartherogue <jasreev@gmail.com>
2018-03-08 00:25:25 -0700

    finished doing the math and moment stuff for time conversions

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ $(document).ready(function() {
         storageBucket: "trainscheduleproject-a6045.appspot.com",
       };
       firebase.initializeApp(config);
+      var rightNow = moment().format('MMMM Do YYYY, HH:mm');
+      console.log(rightNow);
       var trainData = firebase.database();
       var trainName = "";
       var destination = "";
       var frequency = 0;
-      var firstTrain = "HH:mm";
-      var nextArrival = "HH:mm";
+      var firstTrain = ""
+      var nextArrival = "";
       var nextTrain = 0;
     $('#submitButton').on('click', function() {
         event.preventDefault();
         trainName = $("#trainName").val().trim();
         destination = $("#destination").val().trim();
-        firstTrain = $("#firstTime").val().trim();
+        firstTrain = moment($("#firstTime").val().trim(), "HH:mm").subtract(10, "years").format("X");
+        console.log(firstTrain);
         frequency = $("#frequency").val().trim();
         trainData.ref().push({
             trainName: trainName,
@@ -26,6 +29,38 @@ $(document).ready(function() {
             frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
+        $("#trainName").val("");
+        $("#destination").val("");
+        $("#firstTime").val("");
+        $("#frequency").val("");
     });
-
+    trainData.ref().on("child_added", function (snapshot,) {
+        //setting variables from FB info to use in time conversions and populating table.
+        console.log(snapshot.val());
+        trainName = (snapshot.val().trainName);
+        destination = (snapshot.val().destination);
+        firstTrain = (snapshot.val().firstTrain);
+        console.log(firstTrain);
+        frequency = (snapshot.val().frequency);
+        console.log(frequency);
+//Setting Time Conversion, minutes to next arrival and arrival time.
+        var timeDif = moment().diff(moment.unix(firstTrain), "minutes");
+        //finding the remainder to help us determine how far off the next train is. the Minutes place - the remainder should give us how many minutes the next train is. 
+        var timeRemaining = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
+        var minAway = frequency - timeRemaining;
+        console.log(timeDif);
+        console.log(timeRemaining);
+        console.log(minAway);
+        //when will the next train arrive
+        var nextArrival = moment().add(minAway, "m").format("HH:mm A");
+        console.log(nextArrival);
+        var newRow = ("<tr>" +
+                    "<td>" + trainName + "</td>" +
+                    "<td>" + destination + "</td>" +
+                    "<td>" + frequency + "</td>" +
+                    "<td>" + nextArrival + "</td>" +
+                    "<td>" + minAway + "</td>" +
+                    "</tr>");
+                $("#fromFBase").append(newRow);
+    });
 });
